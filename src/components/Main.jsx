a06913da9cb762faf392d84be058175b763a340d
Yash <yashvdkd@gmail.com>
2023-03-11 21:52:56 +0530

    feat: darkmode and copytext added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,10 +16,13 @@ const Main = () => {
     const clearTextHandler = () => {
         setText("");
     }
+    const copyTextHandler = () => {
+        navigator.clipboard.writeText(text)
+    }
 
     return (
         <>
-            <textarea className='w-1/2 h-40 block m-[auto] p-4 border-2 border-blue-100 rounded-lg text-blue-900 font-bold' placeholder='Put your text here and click the button below to see your output.' value={text} onChange={onChangeHandler}></textarea>
+            <textarea className='w-1/2 h-60 block m-[auto] p-4 border-2 border-blue-100 rounded-lg text-blue-900 font-bold' placeholder='Put your text here and click the button below to see your output.' value={text} onChange={onChangeHandler}></textarea>
 
             <div className="w-1/2 m-[auto] flex">
                 <button className='m-[auto] px-4 py-2 bg-blue-100 text-blue-900 font-bold border-2 border-blue-100 rounded-lg hover:shadow' onClick={upperCaseHandler}>To UpperCase</button>
@@ -27,6 +30,8 @@ const Main = () => {
                 <button className='m-[auto] px-4 py-2 bg-blue-100 text-blue-900 font-bold border-2 border-blue-100 rounded-lg hover:shadow' onClick={lowerCaseHandler}>To LowerCase</button>
 
                 <button className='m-[auto] px-4 py-2 bg-blue-100 text-blue-900 font-bold border-2 border-blue-100 rounded-lg hover:shadow' onClick={clearTextHandler}>Clear Text</button>
+
+                <button className='m-[auto] px-4 py-2 bg-blue-100 text-blue-900 font-bold border-2 border-blue-100 rounded-lg hover:shadow' onClick={copyTextHandler}>Copy Text</button>
             </div>
 
             <div className='w-fit m-[auto] text-blue-900 font-bold space-y-2'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,23 @@ const Navbar = (props) => {
     const [emoji, setEmoji] = useState("🌙");
 
     const darkModeHandler =()=>{
-        setEmoji("☀️")
+        if (emoji === "☀️") {
+            setEmoji("🌙")
+        }
+        else {
+            setEmoji("☀️")
+        }
+    }
+
+    let darkmode = {
+        backgroundColor: "black"
+    }
+    let lightmode = {
+        backgroundColor: ""
     }
 
     return (
-        <nav className='h-16 px-20 bg-blue-100 text-blue-900 font-bold flex flex-row justify-between items-center rounded-lg'>
+        <nav className='h-16 px-20 bg-blue-100 text-blue-900 font-bold flex flex-row justify-between items-center rounded-lg shadow transition' style={emoji === "☀️" ? darkmode : lightmode}>
             <h1>{props.title}</h1>
             <button className="" onClick={darkModeHandler}>{emoji}</button>
             <div className="flex space-x-6 font-bold">
