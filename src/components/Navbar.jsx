519f10d87522780aead5adcc7a4b3776f9045542
Yash <yashvdkd@gmail.com>
2023-03-30 02:33:15 +0530

    refactor: hrefs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,9 +28,9 @@ const Navbar = (props) => {
             <nav className='h-16 px-20 bg-blue-100 text-blue-900 font-bold flex flex-row justify-between items-center rounded-b-lg drop-shadow transition' style={props.emoji === "🌙" ? props.lightMode : props.darkMode}>
                 <h1>{props.title}</h1>
                 <div className="flex space-x-6 font-bold cursor-pointer">
-                    <a href="/about">About</a>
-                    <a href="/projects">All Projects</a>
-                    <a href="/contact">Contact</a>
+                    <a href="/">About</a>
+                    <a href="/">All Projects</a>
+                    <a href="/">Contact</a>
                 </div>
             </nav>
         </>
